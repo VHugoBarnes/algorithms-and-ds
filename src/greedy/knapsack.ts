5b7b2a02952702cfa25bc7c70e5c6456d0d61793
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-15 16:45:00 -0600

    add documentation to knapsack

--- src/greedy/knapsack.ts
+++ src/greedy/knapsack.ts
@@ -4,6 +4,23 @@ type Item = {
   ratio?: number;
 }
 
+/**
+ * Represents the knapsack problem where we take a sorted set
+ * of items where we want to take in our knapsack as much value as
+ * possible. There are many items with different weights and values.
+ * 
+ * How to solve this problem:
+ * 
+ * 1. Calculate the ratio of value/weight.
+ * 2. Sort the items from greatest to lowest in terms of ratio.
+ * 3. Fill the knapsack with the item with the most value and we can take.
+ * 4. Repeat the process.
+ * 5. If the next item is greater than the space left, take a fraction.
+ * 
+ * @param items Item[]
+ * @param capacity number
+ * @returns how much value we took from the items
+ */
 export const knapsackFractional = (items: Item[], capacity: number) => {
   //? Calculate the ratio based on value/weight for each item
   items.forEach(item => {
